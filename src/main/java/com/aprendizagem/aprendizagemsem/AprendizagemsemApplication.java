76c1f56716d6785b02f6a7b003b96ccb8dbe47a8
Pddro1 <pedrohcamargon@gmail.com>
2022-08-29 10:55:59 -0300

    Lista de Aprendizagem

---
+++ src/main/java/com/aprendizagem/aprendizagemsem/AprendizagemsemApplication.java
@@ -0,0 +1,13 @@
+package com.aprendizagem.aprendizagemsem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprendizagemsemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprendizagemsemApplication.class, args);
+	}
+
+}
